71d570af8d1287de36f87aca8fcf96128ecfcd51
weitey <92145837+weitey@users.noreply.github.com>
2021-10-08 14:22:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,12 +9,12 @@ const CONFIG = {
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy bụng mỡ ấy ',
     btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
+    btnNo: 'Đừng có thính ',
     question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho bạn <3',
+    btnReply: 'Gửi cho anh <3',
     reply: 'Yêu a Thiện baby kute đẹp trai siêu cấp vô địch vũ trụ <3',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 8h, anh video call nhé.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://https://www.facebook.com/bbxjc //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://https://www.facebook.com/bbxjc
 }
